6b3a5a4519f62b3a0e35df33528f1b680199e259
morullita87 <124706089+morullita87@users.noreply.github.com>
2023-02-21 17:55:47 +0100

    Update recomiendame_paula.py

--- recomiendame_paula.py
+++ recomiendame_paula.py
@@ -9,7 +9,7 @@ pregunta1 = st.radio("Me describiría una persona...",opciones1)
 opciones2 = ("Baja","Medio","Alta")
 pregunta2 = st.radio("¿Cuál es tu nivel socioeconómico?",opciones2)
 
-if pregunta1 == "Introvertida"
+if pregunta1 == "Introvertida" :
 	image1 = Image.open(eleven.jpg)
 	     st.image(image1,caption="Eleven de la serie stranger things")
 
