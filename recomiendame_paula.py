abd246a91359a9f47bb6672103f1e283133e13d3
morullita87 <124706089+morullita87@users.noreply.github.com>
2023-02-21 17:48:52 +0100

    Create recomiendame_paula.py

---
+++ recomiendame_paula.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import random
+
+st.title = ("¿A QUÉ PERSONAJE TE PARECES?")
+
+opciones1 = ("Introvertida","Extrovertida")
+pregunta1 = st.radio("Me describiría una persona...",opciones1)
+
+opciones2 = ("Baja","Medio",""Alta")
+pregunta2 = st.radio("¿Cuál es tu nivel socioeconómico?",opciones2)
+
+if pregunta1 = "Introvertida"
+	image = Image.open()
+
