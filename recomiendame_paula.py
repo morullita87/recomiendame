e52fcdd9fd6f3257d6ec29ac797de6533210c36b
morullita87 <124706089+morullita87@users.noreply.github.com>
2023-02-21 17:54:15 +0100

    Update recomiendame_paula.py

--- recomiendame_paula.py
+++ recomiendame_paula.py
@@ -6,7 +6,7 @@ st.title = ("¿A QUÉ PERSONAJE TE PARECES?")
 opciones1 = ("Introvertida","Extrovertida")
 pregunta1 = st.radio("Me describiría una persona...",opciones1)
 
-opciones2 = ("Baja","Medio",""Alta")
+opciones2 = ("Baja","Medio","Alta")
 pregunta2 = st.radio("¿Cuál es tu nivel socioeconómico?",opciones2)
 
 if pregunta1 = "Introvertida"
