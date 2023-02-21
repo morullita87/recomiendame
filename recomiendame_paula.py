21a20a4c40c24dee7abe84d7ae94308be8cfc104
morullita87 <124706089+morullita87@users.noreply.github.com>
2023-02-21 18:18:49 +0100

    Update recomiendame_paula.py

--- recomiendame_paula.py
+++ recomiendame_paula.py
@@ -1,7 +1,8 @@
 import streamlit as st
+from PIL import Image
 import random
 
-st.title = ("¿A QUÉ PERSONAJE TE PARECES?")
+st.header = ("¿A QUÉ PERSONAJE TE PARECES?")
 
 opciones1 = ("Introvertida","Extrovertida")
 pregunta1 = st.radio("Me describiría una persona...",opciones1)
@@ -10,12 +11,10 @@ opciones2 = ("Baja","Medio","Alta")
 pregunta2 = st.radio("¿Cuál es tu nivel socioeconómico?",opciones2)
 
 if pregunta1 == "Introvertida" :
-	from PIL import Image
 	image = Image.open("eleven.jpg")
 	st.image(image,caption="Eleven de la serie stranger things")
 	
 if pregunta2 == "Extrovertida" :
-	from PIL import Image
 	imageextro = Image.open("bobesponja.jpg")
 	st.image(imageextro,caption="bob esponja")
 
